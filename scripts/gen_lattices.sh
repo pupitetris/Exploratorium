4446de7d9d028062b4ba1a65212a6af294d03a63
Arturo Espinosa <pupitetris@yahoo.com>
2023-11-02 09:39:07 -0600

    scripts/gen_lattices.sh: don't cd. Be location-agnositc.
    The cd actually broke the script

--- scripts/gen_lattices.sh
+++ scripts/gen_lattices.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 SCRIPTDIR=$(dirname "$0")
-cd "$SCRIPTDIR/.."
 
 DBFILE=$1
 
 while read -r lang; do
   while read -r diagram; do
-    fname=site/theories-$lang/$diagram/lattice.json
+    fname="$SCRIPTDIR"/../site/theories-$lang/$diagram/lattice.json
     echo "Generating $fname"
-    "$SCRIPTDIR"/gen_lattice.sh "$DBFILE" $diagram $lang > $fname
+    "$SCRIPTDIR"/gen_lattice.sh "$DBFILE" $diagram $lang > "$fname"
   done < <(sqlite3 "$DBFILE" "SELECT code FROM diagram")
 done < <(sqlite3 "$DBFILE" "SELECT lang_code FROM lang")
